aeccfe308f0f73667f31fbc6714b2d773360db4f
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-08-07 07:25:20 +0400

    search component added

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './Search.module.scss'
+
+const Search = () => {
+    return ( 
+        <div className={styles.root}>
+            <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
+            <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <input className={styles.input} placeholder='Поиск пиццы' />
+
+        </div>
+     );
+}
+ 
+export default Search;
