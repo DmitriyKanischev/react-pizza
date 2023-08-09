6b9e3b5af71c60552bf23b1ad7efe57263875542
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-08-09 21:05:09 +0400

    Search component edited

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import Cart from './pages/Cart';
 import {Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [searchInput, setSearchInput] = React.useState('')
+  console.log(searchInput)
   return (
     <>
       <div className="wrapper">
-      <Header/>
+      <Header searchInput={searchInput} setSearchInput={setSearchInput} />
       <div className="content">
       <Routes>
         <Route path='/' element={<Home/>}/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg'
 import Search from './Search';
 
-const Header = () => {
+const Header = ({searchInput, setSearchInput}) => {
     return ( 
         <div className="header">
         <div className="container">
@@ -15,7 +15,7 @@ const Header = () => {
               </div>
             </div>
           </Link>
-          <Search />
+          <Search searchInput={searchInput} setSearchInput={setSearchInput} />
           <div className="header__cart">
             <Link to="/cart" className="button button--cart">
               <span>520 ₽</span>
